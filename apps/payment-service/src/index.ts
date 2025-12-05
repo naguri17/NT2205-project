34cfc6cf667a42ac43927f2a980411527af497f8
Hoang Loc <locdinh.public@gmail.com>
2025-12-06 04:40:49 +0700

    chore: create common type

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { uptime } from "process";
 import { authMiddleware } from "./middleware/auth.js";
 import { Variables } from "./types/hono.js";
+import stripe from "./utils/stripe.js";
 
 const app = new Hono<{ Variables: Variables }>();
 
@@ -14,6 +15,27 @@ app.get("/test", authMiddleware, (c) => {
     message: "Payment service authenticated",
   });
 });
+
+// app.post("/create-stripe-product", async (c) => {
+//   const res = await stripe.products.create({
+//     id: "123",
+//     name: "Test Product",
+//     default_price_data: {
+//       currency: "usd",
+//       unit_amount: 10 * 100,
+//     },
+//   });
+
+//   return c.json(res);
+// });
+
+// app.get("/stripe-product-price", async (c) => {
+//   const res = await stripe.prices.list({
+//     product: "123",
+//   });
+
+//   return c.json(res);
+// });
 
 app.get("/health", (c) => {
   return c.json({
