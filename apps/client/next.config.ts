1cb811734665d742eca0156f33402c109a1e93e7
Hoi Truong <hoitrnd107@gmail.com>
2025-12-16 10:20:49 +0700

    chore: add eslint configuration to ignore during builds in admin and client next.config.ts

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@repo/types"],
+  eslint: { ignoreDuringBuilds: true }
 };
 
 export default nextConfig;
